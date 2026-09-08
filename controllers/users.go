3d16e1ec9cd9407b84dbfa0893ea2e39fe9aa940
sangodare <sangodare.toyeeb@gmail.com>
2026-09-08 04:51:47 +0100

    feat: add signin page

--- controllers/users.go
+++ controllers/users.go
@@ -4,18 +4,22 @@ import "net/http"
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
+	u.Templates.New.Execute(w, r, nil)
+
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
 	var data struct {
 		Email string
 	}
 
 	data.Email = r.FormValue("email")
-	u.Templates.New.Execute(w, r, data)
+	u.Templates.SignIn.Execute(w, r, data)
 
 }
-
-
--- main.go
+++ main.go
@@ -30,12 +30,14 @@ func server() error {
 
 	//Templates
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.html" ))
+usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.html" ))
 
 	//Routes
 	mux := http.NewServeMux()
 	mux.Handle("GET /assets/", http.StripPrefix("/assets/", cssFile))
 	mux.Handle("GET /", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.html", "reusable.html"))))
 	mux.HandleFunc("GET /signup", usersC.New)
+	mux.HandleFunc("GET /signin", usersC.SignIn)
 
 	//Start up the Server
 	fmt.Println("starting up the server on port 4000")
